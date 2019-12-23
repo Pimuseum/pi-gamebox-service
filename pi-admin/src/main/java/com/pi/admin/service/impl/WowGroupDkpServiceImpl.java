9fbf920bf8b395b54dcb5ba8c24f1da0704fca01
gaotang <gaotang@bubugao.com>
2019-12-23 17:25:30 +0800

    wowDKP服务 开发12.23

--- pi-admin/src/main/java/com/pi/admin/service/impl/WowGroupDkpServiceImpl.java
+++ pi-admin/src/main/java/com/pi/admin/service/impl/WowGroupDkpServiceImpl.java
@@ -1,7 +1,11 @@
 package com.pi.admin.service.impl;
 
+import com.pi.admin.model.wow.DkpLog;
+import com.pi.admin.model.wow.WowGroup;
 import com.pi.admin.service.WowGroupDkpService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author Gturn
@@ -12,4 +16,43 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class WowGroupDkpServiceImpl implements WowGroupDkpService {
+    @Override
+    public Integer openDkpGroup(WowGroup wowGroup) {
+        return null;
+    }
+
+    @Override
+    public Integer updateDkpGroup(WowGroup wowGroup) {
+        return null;
+    }
+
+    @Override
+    public Integer cancelDkpGroup(Long groupId, Boolean flag) {
+        return null;
+    }
+
+    @Override
+    public Integer delDkpGroup(Long groupId, Boolean flag) {
+        return null;
+    }
+
+    @Override
+    public List<WowGroup> QueryDkpGroup(String note) {
+        return null;
+    }
+
+    @Override
+    public WowGroup getDkpGroupDetail(Long gourpId) {
+        return null;
+    }
+
+    @Override
+    public List<DkpLog> settlementGroupDkpPreview(Long gourpId) {
+        return null;
+    }
+
+    @Override
+    public void settlementGroupDkp(List<DkpLog> dkpLogs, String note) {
+
+    }
 }
